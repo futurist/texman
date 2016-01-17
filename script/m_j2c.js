45caf14ae4985e28bad419ba46c274d1a461aa16
1111hui <26634873@qq.com>
2016-01-17 15:52:25 +0800

    CHANGE: all param of apis rechecked with consistency

--- script/m_j2c.js
+++ script/m_j2c.js
@@ -2,17 +2,19 @@ import j2c from './j2c.es6'
 import util from 'util_extend_exclude'
 // import m from 'mithril'
 // have to manually set m ref when required
-var m= window&&window.m||function(){}
+var m= typeof window!='undefined'&&window.m||null
 
 var OBJECT = "[object Object]", ARRAY = "[object Array]", STRING = "[object String]", REGEXP="[object RegExp]";
 var type = {}.toString;
 
-var DEFAULT_NS = 'default'
-var namespace = DEFAULT_NS
 var j2cGlobal = {}
 var domClassMap = []
 
-j2cGlobal[DEFAULT_NS] = {};
+m_j2c.DEFAULT_NS = 'default';
+m_j2c.j2c = j2c;
+var namespace = m_j2c.DEFAULT_NS
+
+j2cGlobal[m_j2c.DEFAULT_NS] = {};
 
 var isBrowser = typeof document==='object' && document && document instanceof Node;
 
@@ -54,11 +56,6 @@ function addStyleToHead(styleObj, name) {
 }
 
 function intervdom (sheet, vdom){
-	if(!vdom)return vdom;
-	// if it's a mithril compoent return; TODO: change m.component's vdom
-	if(isComponent(vdom)){
-		return vdom
-	}
 	if(vdom.attrs&&vdom.attrs.className){
 		vdom.attrs.className = vdom.attrs.className.split(/\s+/).map(function(c){
 			var g = c.match(/global\((.*)\)/);
@@ -71,9 +68,9 @@ function intervdom (sheet, vdom){
 	return vdom
 }
 function applyStyle (sheet, vdom){
-	if( type.call(vdom)===OBJECT&&vdom.subtree=='retain' ) return vdom
 	if( type.call(vdom)===ARRAY ) return vdom.map( function(v){ return applyStyle(sheet, v) } );
-	return [intervdom(sheet, vdom)]
+	if( type.call(vdom)!==OBJECT) return vdom
+	return intervdom(sheet, vdom)
 }
 
 function removeDom (styleObj) {
@@ -83,64 +80,49 @@ function removeDom (styleObj) {
 	delete styleObj.dom
 }
 
-function parseNSName(nsName=""){
-	var arr = nsName.split(':')
-	if(arr.length<=1)return [namespace, nsName];
-	var ns = arr.shift()||namespace
-	return [ns, arr.join(':') ]
-}
-
 function m_j2c(ns, name, vdom) {
 	var args = arguments;
-	// usage: m_j2c() will return all j2cStore
-	if(args.length===0) return j2cGlobal;
+	if(args.length===0) return j2cGlobal;	//m_j2c()
 	if(args.length===1){
 		//if it's mithril ref
-		if(typeof ns=='function' && ns.prop){
+		if(typeof ns=='function' && ns.prop){	//m_j2c(m)
 			m=ns; return m_j2c;
 		}
-		name=ns, ns=namespace;
+		name=ns, ns=namespace;	//m_j2c('name')-> curNS,name
 	}
-	if(typeof args[1]=='object') vdom=name, name=ns, ns=namespace;
-	ns=ns||DEFAULT_NS
+	if(typeof args[1]=='object') vdom=name, name=ns, ns=namespace;	//m_j2c('name', vdom)-> curNS,name(vdom)
+	ns=ns||m_j2c.DEFAULT_NS	//m_j2c('', 'name') -> DEFAULT_NS, name
 
-	if(!name) return j2cGlobal[ns];
-	else if(!vdom) return j2cGlobal[ns]&&j2cGlobal[ns][name];
+	if(!name) return j2cGlobal[ns];	//m_j2c('')->curNS  ||  m_j2c('', '')->DEFAULT_NS
+	else if(!vdom) return j2cGlobal[ns]&&j2cGlobal[ns][name];	//m_j2c('name')->curNS,name &&  m_j2c('', 'name')->DEFAULT_NS,name
 
-	if( isElement(name)||isElement(vdom) ) return m_j2c.applyClass.call(this, vdom||name, ns, name);
-	var j2cStore = j2cGlobal[ns]
-	var styleObj = j2cStore[name]
-	// usage: m_j2c('name') will return all j2cStore['name']
-	if(!vdom) return styleObj;
+	if( type.call(name)==OBJECT ){
+		//support {'.item li':{float:left}} as name
+		var tempName='temp_'+Date.now()+Math.random()
+		m_j2c.add(ns, tempName, name)
+		name=tempName
+	}
+	if( isElement(vdom) ) return m_j2c.applyClass(ns, name, vdom);
+	var styleObj = j2cGlobal[ns][name]
 	// usage: m_j2c('name', mithril_v_dom) will add style to vdom and create <style> for it, cache style_dom
 	if( !styleObj || !styleObj.sheet ) return applyStyle({}, vdom);
-	if(isComponent(vdom)){
-		return vdom
-	}
-	var sheet = styleObj.sheet;
-	// here we can insert into header instead of vdom
-	addStyleToHead(styleObj, name)
-
 	// Known Issue: the dom will always re-created when pass to mithril, so we set below to skip next redraw()
 	// m.redraw.strategy('none')
-	var ret = applyStyle(sheet, vdom)
+	var ret = applyStyle(styleObj.sheet, vdom)
 	// console.log(ret)
 	return  ret
 }
-m_j2c.DEFAULT_NS = DEFAULT_NS;
-m_j2c.j2c = j2c;
-m_j2c.m = m;
 m_j2c.removeNS = function( ns ){
 	// DEFAULT_NS cannot be removed
-	if(!ns||ns===DEFAULT_NS) return;
+	if(!ns||ns===m_j2c.DEFAULT_NS) return;
 	if(ns===namespace){
 		var j2cStore = j2cGlobal[ns]
 		for(var i in j2cStore){
 			removeDom( j2cStore[i] )
 		}
-		m_j2c.applyClass(null,null,false)
-		namespace = DEFAULT_NS
-		m.redraw()
+		m_j2c.revertClass()
+		namespace = m_j2c.DEFAULT_NS
+		m&&m.redraw()
 	}
 	delete j2cGlobal[ns]
 	return m_j2c
@@ -150,37 +132,35 @@ m_j2c.getNS = function( ns ){
 }
 m_j2c.setNS = function( ns ){
 	var j2cStore = j2cGlobal[namespace]
-	ns = ns||DEFAULT_NS
+	ns = ns||m_j2c.DEFAULT_NS
 	namespace = ns;
 	for(var i in j2cStore){
 		removeDom( j2cStore[i] )
 	}
-	m_j2c.applyClass(null,null,false)
+	m_j2c.revertClass()
 
 	if(!j2cGlobal[namespace]) j2cGlobal[namespace] = j2cStore = {} ;
 	else j2cStore = j2cGlobal[namespace];
 	for(i in j2cStore){
 		m_j2c.add( i, j2cStore[i].cssObj )
 	}
-	m.redraw()
+	m&&m.redraw()
 
 	return m_j2c
 }
-m_j2c.get = m_j2c;
 m_j2c.add = function( ns, name, cssObj ) {
 	var args = arguments;
 	if(args.length===0) return j2cGlobal[namespace];
 	if(args.length===1) name=ns, ns=namespace;
 	if(typeof args[1]=='object') cssObj=name, name=ns, ns=namespace;
-	ns=ns||DEFAULT_NS
+	ns=ns||m_j2c.DEFAULT_NS
 
 	if(!name)return j2cGlobal[ns];
 	else if(!cssObj)return j2cGlobal[ns]&&j2cGlobal[ns][name];
-	
+
 	var j2cStore = j2cGlobal[ns]
 	if(!j2cStore) j2cGlobal[ns]={}
 	var styleObj
-	var isHead = name.indexOf('<head')===0;
 	if(!j2cStore[name]){
 		styleObj = j2cStore[name] = { cssObj:cssObj, version:0, sheet:j2c.sheet(cssObj) };
 	} else {
@@ -189,9 +169,13 @@ m_j2c.add = function( ns, name, cssObj ) {
 		styleObj.sheet = j2c.sheet(styleObj.cssObj);
 		styleObj.version++
 	}
-	if( isHead ) addStyleToHead(styleObj, name)
-	else if( styleObj.dom ) m.redraw();
+	addStyleToHead(styleObj, name)
+	domClassMap.forEach(function(v){
+		if(v.ns==ns&&v.name==name){
 
+		}
+	})
+	m&&m.redraw();
 	return j2cStore[name];
 }
 m_j2c.remove = function(ns, name, cssObj) {
@@ -199,7 +183,7 @@ m_j2c.remove = function(ns, name, cssObj) {
 	if(args.length===0) return j2cGlobal[namespace];
 	if(args.length===1) name=ns, ns=namespace;
 	if(typeof args[1]=='object') cssObj=name, name=ns, ns=namespace;
-	ns=ns||DEFAULT_NS
+	ns=ns||m_j2c.DEFAULT_NS
 
 	if(!name)return j2cGlobal[ns];
 	else if(!cssObj)return j2cGlobal[ns]&&j2cGlobal[ns][name];
@@ -207,7 +191,6 @@ m_j2c.remove = function(ns, name, cssObj) {
 	var j2cStore = j2cGlobal[ns]
 	if(!j2cStore) return;
 
-	var isHead = name.indexOf('<head')===0;
 	var styleObj = j2cStore[name];
 	if(!cssObj){
 		delete j2cStore[name]
@@ -216,23 +199,19 @@ m_j2c.remove = function(ns, name, cssObj) {
 		styleObj.sheet = j2c.sheet(styleObj.cssObj);
 		styleObj.version++
 	}
-	if( isHead ) {
-		cssObj
-		? addStyleToHead(styleObj, name)
-		: removeDom( styleObj )
-	}
-	else if( styleObj.dom ) m.redraw();
-
+	cssObj
+	? addStyleToHead(styleObj, name)
+	: removeDom( styleObj );
+	m&&m.redraw();
 	return styleObj
 }
 m_j2c.getClass = function (ns, name) {
 	var args = arguments
-	if(args.length===0) return j2cGlobal[namespace];
-	if(args.length===1) name=ns, ns=namespace
-	ns=ns||DEFAULT_NS
+	if(args.length===0) return j2cGlobal[namespace];	//getClass()->curNS
+	if(args.length===1) name=ns, ns=namespace	//getClass('name')->curNS,name
+	ns=ns||m_j2c.DEFAULT_NS	//getClass('', 'name')->DEFAULT_NS,name
 	var sheet, list = {}, store= j2cGlobal[ns]
-	if(!store) return;
-	name = name||/./
+	if(!store||!name) return;
 	for(var i in store){
 		// tutpoint: string.match(undefined) ?
 		if( (sheet=store[i].sheet) && ( type.call(name)==REGEXP ? i.match(name) : i==name ) ){
@@ -241,43 +220,67 @@ m_j2c.getClass = function (ns, name) {
 	}
 	return list;
 }
-m_j2c.getClassMap = function () {
+
+var _addClassToDom = function(dom, domRange, ns, name, isAdd) {
+	var pos, c = dom.className&&dom.className.split(/\s+/)
+    if(c) dom.className = c.map(function(v){
+    	if( isAdd===false ) {
+    		if((pos=findDom(dom))!==-1){
+    			domRange&&domRange.push( domClassMap[pos] );
+	    		var old = domClassMap[pos].original
+	    		// domClassMap.splice(pos,1)
+	    		return  domClassMap[pos].j2c==v?old:v
+    		}else{
+    			return v
+    		}
+    	}else{
+    		var list = m_j2c.getClass(ns, name)
+    		if(!list)return
+	    	var j2cClass = list[v]
+	    	if(j2cClass){
+	    		var obj={ ns:ns, name:name, dom:dom, original:v, j2c:j2cClass }
+	    		domRange&&domRange.push( obj );
+	    		domClassMap.push( obj );
+	    	}
+	    	return j2cClass||v
+    	}
+    }).join(' ')
+}
+
+m_j2c.domMap = function (){
 	return domClassMap
 }
-m_j2c.applyClass = function (ns, name, target, isAdd){
-	if(! isBrowser) return;
-	var args = arguments;
-	if(args.length===0) return m_j2c.getClass.apply(this, args);
-	if(args.length===1) name=ns, ns=namespace;
-	if(typeof args[1]=='object') isAdd=target, target=name, name=ns, ns=namespace;
-	ns=ns||DEFAULT_NS
-
-	var list = m_j2c.getClass(name)
-	if(!list) return;
-	var _addClassToDom = function(dom){
-		var pos, c = dom.className&&dom.className.split(/\s+/)
-	    if(c) dom.className = c.map(function(v){
-	    	if( isAdd===false ) {
-	    		if((pos=findDom(dom))!==-1){
-		    		var old = domClassMap[pos].original
-		    		domClassMap.splice(pos,1)
-		    		return old
-	    		}else{
-	    			return v
-	    		}
-	    	}else{
-		    	var j2cClass = list[v]
-		    	if(j2cClass) domClassMap.push( { dom:dom, original:v } );
-		    	return j2cClass||v
-	    	}
-	    }).join(' ')
-	}
-	if( !isElement(target) ) target=document.body;
-	_addClassToDom(target)
+m_j2c.revertClass = function (target){
+	var domRange=[]
+	if( !target||!isElement(target) )target=document.body;
+	_addClassToDom(target,domRange,null,null,false)
 	var items = target.getElementsByTagName("*")
 	for (var i = items.length; i--;) {
-	    _addClassToDom(items[i])
+	    _addClassToDom(items[i],domRange,null,null,false)
 	}
+	return domRange
+}
+m_j2c.applyClass = function (ns, name, target){
+	if(! isBrowser) return;
+	var args = arguments;
+	if(typeof args[1]=='object') target=name, name=ns, ns=namespace;
+	if(name===undefined) name=ns, ns=namespace;
+	if( !target||!isElement(target) )target=document.body;
+	ns=ns||m_j2c.DEFAULT_NS
+	console.log(ns, name, target)
+	if( type.call(name)==OBJECT ){
+		//support {'.item li':{float:left}} as name
+		var tempName='temp_'+Date.now()+Math.random()
+		m_j2c.add(ns, tempName, name)
+		name=tempName
+	}
+	var domRange=[]
+	_addClassToDom(target, domRange, ns, name)
+	var items = target.getElementsByTagName("*")
+	for (var i = items.length; i--;) {
+	    _addClassToDom(items[i], domRange, ns, name)
+	}
+	return domRange
 }
 
 export default m_j2c
